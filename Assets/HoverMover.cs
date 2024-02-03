3c7f591e9292208506c48c7b2877c54cfd7e88a3
Bogdan Mocanu <bogdan.game.development@gmail.com>
2024-02-03 20:51:38 +0100

    Added gizmos for hover

--- Assets/HoverMover.cs
+++ Assets/HoverMover.cs
@@ -1,6 +1,7 @@
 using Bogadanul.Assets.Scripts.Utility;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UIElements;
@@ -10,6 +11,8 @@ public class HoverMover : MonoBehaviour
     private CursorController cursorController;
 
     private Camera cam;
+    [SerializeField] private Vector2 minLimits;
+    [SerializeField] private Vector2 maxLimits;
 
     private void Start()
     {
@@ -31,6 +34,13 @@ public class HoverMover : MonoBehaviour
     private void Move(Vector3 pos)
     {
         pos = cam.ScreenToWorldPoint(pos);
+        if (pos.x < minLimits.x || pos.x > maxLimits.x || pos.y < minLimits.y || pos.y > maxLimits.y)
+            return;
         transform.position = new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(new Vector3(1.5f, 2f, 0), minLimits + maxLimits);
     }
 }
