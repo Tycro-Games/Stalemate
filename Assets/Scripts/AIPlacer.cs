a9d892d502fe9295c2b0a564cce7e60550c6e5d3
Bogdan Mocanu <bogdan.game.development@gmail.com>
2024-02-18 21:11:58 +0100

    AI scene

--- Assets/Scripts/AIPlacer.cs
+++ Assets/Scripts/AIPlacer.cs
@@ -47,6 +47,25 @@ public class AIPlacer : MonoBehaviour
         onFogOfWar?.Invoke();
     }
 
+    public void AIFogOfWarOtherSide()
+    {
+        //Debug.Log("AIPlacer.AIFogOfWar");
+        weight = RedBlueTurn.maxPoints;
+        // between 1 and 4
+        var countEnemies = Random.Range(1 + weight / 6, Mathf.Min(weight, 5));
+
+
+        var isRed = !RedBlueTurn.IsRedFirst();
+        var fogOfWar = isRed ? fogOfWarBlue : fogOfWarRed;
+        positions = new List<UnitRenderer>(board.GetSquares(isRed ? SquareType.BLUE : SquareType.RED));
+        countEnemies = Mathf.Min(countEnemies, positions.Count);
+        GetListOfPositions(ref positions, countEnemies);
+        //Debug.Log(positions);
+        foreach (var unitRenderer in positions) unitRenderer.SetUnitSettings(fogOfWar);
+
+        // onFogOfWar?.Invoke();
+    }
+
     public void PlaceEnemies()
     {
         weight = Mathf.Min(weight, positions.Count * 5);
@@ -101,6 +120,62 @@ public class AIPlacer : MonoBehaviour
         }
 
         onEnemyEndTurn?.Invoke();
+    }
+
+    public void PlaceEnemiesOtherSide()
+    {
+        weight = Mathf.Min(weight, positions.Count * 5);
+        if (positions.Count == 0)
+        {
+            onEnemyEndTurn?.Invoke();
+            return;
+        }
+
+        var enemyList = !RedBlueTurn.IsRedFirst() ? blueUnits : redUnits;
+        var indexEnemy = new List<int>();
+        var unitRenderers = new List<UnitRenderer>();
+        while (positions.Count > 0 && weight > 0)
+        {
+            var min = positions.Count;
+            var max = weight - (positions.Count - 1);
+            if (min > max)
+                min = max;
+            var enemy = ChooseEnemy(min, max, enemyList);
+            var currentW = enemy.cost;
+            weight -= currentW;
+            var unitRenderer = positions[Random.Range(0, positions.Count)];
+            indexEnemy.Add(currentW - 1);
+            unitRenderers.Add(unitRenderer);
+            positions.Remove(unitRenderer);
+        }
+
+        if (weight > 0)
+        {
+            var enemyIndicies = indexEnemy;
+            enemyIndicies = enemyIndicies.Where(en => en < 5).ToList();
+
+            while (weight > 0)
+            {
+                enemyIndicies[Random.Range(0, enemyIndicies.Count)]++;
+                Debug.Log(enemyIndicies);
+                weight--;
+                Debug.Log("Remaining weight:" + weight);
+            }
+
+            for (var i = 0; i < indexEnemy.Count; i++)
+                if (unitRenderers[i].GetUnitSettings().cost < 5)
+                    SetUnitRenderer(unitRenderers[i], enemyList[enemyIndicies[i]]);
+                else
+                    SetUnitRenderer(unitRenderers[i], enemyList[enemyList.Count - 1]);
+        }
+        else
+
+        {
+            for (var i = 0; i < unitRenderers.Count; i++)
+                SetUnitRenderer(unitRenderers[i], enemyList[indexEnemy[i]]);
+        }
+
+        //  onEnemyEndTurn?.Invoke();
     }
 
     private void SetUnitRenderer(UnitRenderer unitRenderer, ScriptableUnitSettings settings)
