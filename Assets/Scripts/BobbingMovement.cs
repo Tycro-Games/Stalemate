fc2b52f9d216abdb2ee32d08e5f3b47bffc3cb9c
Jasper <138149547+Spel16@users.noreply.github.com>
2024-07-01 16:39:28 +0200

    CHANGED Ordering of AI placement
    AI now places after the player does

--- Assets/Scripts/BobbingMovement.cs
+++ Assets/Scripts/BobbingMovement.cs
@@ -7,22 +7,22 @@ public class Unit : MonoBehaviour
     private GameObject EmptyParent;
     public float HeightOffset;
     public float BobbingSpeed;
-    // Start is called before the first frame update
-    void OnEnable()
-    {//Create an empty Parent for the Unit
+    void OnEnable() 
+    {
+        //Create an empty Parent for the Unit
         EmptyParent = new GameObject($"{gameObject.name} Offset Parent");
         EmptyParent.transform.parent = gameObject.transform.parent;
         gameObject.transform.parent = EmptyParent.transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        EmptyParent.transform.position = new(EmptyParent.transform.position.x, CalcOffset(), EmptyParent.transform.position.z);
+        EmptyParent.transform.position = new Vector3(EmptyParent.transform.position.x, CalcOffset(), EmptyParent.transform.position.z);
     }
 
     float CalcOffset()
     {
-        return HeightOffset * Mathf.Sin(BobbingSpeed * (float)Time.timeAsDouble);
+        //Caclulate the Offset
+        return HeightOffset * Mathf.Sin(BobbingSpeed * Time.time);
     }
 }
