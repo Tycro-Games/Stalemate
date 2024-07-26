ce7c99e257fd2cbdf3300379d024960ac0885603
Bogdan Mocanu <bogdan.game.development@gmail.com>
2024-07-26 09:28:18 +0300

    Fixed the peak bug

--- Assets/Scripts/CellFinder.cs
+++ Assets/Scripts/CellFinder.cs
@@ -4,14 +4,20 @@ public class CellFinder : MonoBehaviour
 {
     [SerializeField] private LayerMask layerToPlace = 0;
     private Camera cam;
-
+    private bool isActive = true;
     private void Start()
     {
         cam = Camera.main;
     }
 
+    public void SwitchActive()
+    {
+        isActive = !isActive;
+    }
     public GameObject NodeFromInput(Vector2 position)
     {
+        if (!isActive)
+            return null;
         if (Physics.Raycast(cam.ScreenPointToRay(position), out var hit, 50, layerToPlace))
             return hit.collider.gameObject;
         return null;
